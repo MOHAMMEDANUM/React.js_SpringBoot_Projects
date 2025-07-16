6be000c0267cb788a8bb618d5398f15f0b2c9947
Mohammed Anum <160215305+MOHAMMEDANUM@users.noreply.github.com>
2025-07-16 21:49:44 +0530

    Made some basic changes in the code

--- dealmart/src/components/Login/SignIn.jsx
+++ dealmart/src/components/Login/SignIn.jsx
@@ -5,27 +5,19 @@ import { Link } from "react-router-dom";
  function SignIn(props) {
   const emailRef = useRef();
   const passwordRef = useRef();
-  // const loginDataForm = useRef([]);
-
-      
-    
 
   const handleLogin = async (e) => {
     e.preventDefault();
-    const email = emailRef.current.value;
-    const password = passwordRef.current.value;
-    // const loginData = loginDataForm.current.value;
+
+    const signInemail = emailRef.current.value;
+    const signInpassword = passwordRef.current.value;
 
     // You can connect this with backend auth API
     console.log("Login Attempt:", { email, password });
-    // console.log("Login Attempt:", { loginData});
-
-    
 
       let res
       let fetchSignUpData = await fetch("http://localhost:8080/login")
       res = await fetchSignUpData.json()
-      console.log(res);
 
       console.log(res[0].email);
       console.log(res[0].password);
@@ -33,29 +25,19 @@ import { Link } from "react-router-dom";
       let signupEmail=res[0].email
       let signupPassword = res[0].password
 
-
-      if(signupEmail===email && signupPassword===password){
+      if(signupEmail===signInemail && signupPassword===signInpassword){
         console.log("Login successfully Beta");
-        
+        window.location.href="Admin"
         props.setFlagAdminLogin(true)
-
       }
       else{
         console.log("Nalla Error");
-        
       }
       
-
-
-
-
-
-
-
     // Clear fields
     emailRef.current.value = "";
     passwordRef.current.value = "";
-    // loginDataForm.current.value="";
+
   };
 
   return (
@@ -78,5 +60,4 @@ import { Link } from "react-router-dom";
     </div>
   );
 }
-
 export default SignIn;
